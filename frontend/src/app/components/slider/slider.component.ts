b22403ec122e5269f663a9712b1853a90b9898dd
01648408 <e01648408@student.tuwien.ac.at>
2019-12-15 11:17:55 +0100

    Showing topten events on startpage

--- frontend/src/app/components/slider/slider.component.ts
+++ frontend/src/app/components/slider/slider.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/services/event.service';
+import { GlobalEvent } from 'src/app/dtos/global-event';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'slider',
@@ -7,18 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
+  topTenEvents: GlobalEvent[];
+  error: boolean = false;
+  errorMessage: string = 'There was a problem loading the topTenEvents';
 
   ngOnInit() {
+    this.loadTopTenEvents();
   }
 
+  loadTopTenEvents() {
+    this.eventService.getTopTenEvents().subscribe(
+      (retTopTenEvents: GlobalEvent[]) => {
+        this.topTenEvents = retTopTenEvents;
+      },
+      error => {
+        this.defaultServiceErrorHandling(error);
+      }
+    );
+  }
+
+
+
   swiperight(): void {
-    let element = document.getElementById('slider');
+    const element = document.getElementById('slider');
     element.scroll({top: 0, left: 10000, behavior: 'smooth'});
   }
 
   swipeleft(): void {
-    let element = document.getElementById('slider');
+    const element = document.getElementById('slider');
     element.scroll({top: 0, left: 0, behavior: 'smooth'});
   }
+
+private defaultServiceErrorHandling(error: any) {
+  console.log(error);
+  this.error = true;
+  if (error.status === 0) {
+    // If status is 0, the backend is probably down
+    this.errorMessage = 'The backend seems not to be reachable';
+  } else if (error.error.message === 'No message available') {
+    // If no detailed error message is provided, fall back to the simple error name
+    this.errorMessage = error.error.error;
+  } else {
+    this.errorMessage = error.error.message;
+  }
+}
+
 }
