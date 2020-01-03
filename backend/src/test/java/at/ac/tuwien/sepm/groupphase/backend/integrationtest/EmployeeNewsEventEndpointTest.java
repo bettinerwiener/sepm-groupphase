4b8809201bcc7edf91435b0c7c43759597b309f6
Christian <e0121092@student.tuwien.ac.at>
2020-01-03 13:44:03 +0100

    EmployeeNewsEventEdnpointTest with findAll test

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/EmployeeNewsEventEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/EmployeeNewsEventEndpointTest.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.groupphase.backend.integrationtest;
 
 import at.ac.tuwien.sepm.groupphase.backend.basetest.EmployeeNewsEventTestData;
 import at.ac.tuwien.sepm.groupphase.backend.config.properties.SecurityProperties;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.EmployeeNewsEventDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.EmployeeNewsEventMapper;
 import at.ac.tuwien.sepm.groupphase.backend.security.JwtTokenizer;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,12 +11,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -41,12 +51,29 @@ public class EmployeeNewsEventEndpointTest implements EmployeeNewsEventTestData 
 
 
     @Test
-    public void creatingNewsReturns201() throws Exception {
+    public void creatingEventNewsReturns201() throws Exception {
         this.mockMvc.perform(post(EVENT_NEWS_BASE_URI)
             .header(securityProperties.getAuthHeader(), jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES))
             .contentType(MediaType.APPLICATION_JSON)
             .content(POST_EVENT_NEWS_JSON)
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void findAllEventNewsReturns4() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get(EVENT_NEWS_BASE_URI)
+            .header(securityProperties.getAuthHeader(), jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES)))
+            .andDo(print())
+            .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType());
+
+        List<EmployeeNewsEventDto> eventNews = Arrays.asList(objectMapper.readValue(response.getContentAsString(),
+            EmployeeNewsEventDto[].class));
+
+        assertEquals(2, eventNews.size());
     }
 }
