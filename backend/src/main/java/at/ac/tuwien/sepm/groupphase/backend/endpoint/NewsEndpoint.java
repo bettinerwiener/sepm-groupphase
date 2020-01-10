e871a3ffc05c0f541edb311ad17b82d39128d887
Christian <e0121092@student.tuwien.ac.at>
2020-01-10 01:22:43 +0100

    image upload is working with blob and formdata

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/NewsEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/NewsEndpoint.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Authorization;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Slf4j
@@ -29,6 +30,13 @@ public class NewsEndpoint {
     public NewsDto create(@RequestBody NewsDto newsDto) {
         News toSave = this.newsMapper.newsDtoToNews(newsDto);
         return this.newsMapper.newsToNewsDto(this.newsService.save(toSave));
+    }
+
+    @PostMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Create a news entry", authorizations = {@Authorization(value = "apiKey")})
+    public NewsDto update(@RequestParam("image") MultipartFile image, @PathVariable("id") Long id)  {
+        return this.newsMapper.newsToNewsDto(this.newsService.updateWithImage(id, image));
     }
 
 
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/NewsService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/NewsService.java
@@ -4,6 +4,7 @@ package at.ac.tuwien.sepm.groupphase.backend.service;
 import at.ac.tuwien.sepm.groupphase.backend.entity.News;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotCreatedException;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface NewsService {
 
@@ -22,4 +23,6 @@ public interface NewsService {
      * @throws NotFoundException in case no news with @param id is found or the database access fails
      */
     News findById(Long id) throws NotFoundException;
+
+    News updateWithImage(Long id, MultipartFile image) throws NotFoundException;
 }
