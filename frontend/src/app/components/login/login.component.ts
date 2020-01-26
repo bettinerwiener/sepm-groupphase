22e9e75c8fdfb08fad9d32ae9546298de01e9039
01648408 <e01648408@student.tuwien.ac.at>
2020-01-26 22:16:41 +0100

    After login redirect to news

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -48,7 +48,7 @@ export class LoginComponent implements OnInit {
     this.authService.loginUser(authRequest).subscribe(
       () => {
         console.log('Successfully logged in user: ' + authRequest.email);
-        this.router.navigate(['/']).then(() => {
+        this.router.navigate(['/news']).then(() => {
           window.location.reload();
         });
       },
